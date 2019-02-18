47458df86e08b304bb8e8c65287d326e697f9dd9
AMINE <AMINE@DESKTOP-N3DQ590>
2019-02-18 13:29:35 +0100

    jenkins SECON D COMM

--- src/main/java/com/sir2/commandeapi2/CommandeApi2Application.java
+++ src/main/java/com/sir2/commandeapi2/CommandeApi2Application.java
@@ -8,7 +8,6 @@ public class CommandeApi2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommandeApi2Application.class, args);
-                System.out.println("bonjour");
 	}
 
 }
